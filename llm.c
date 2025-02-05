3f8aa8deef594172af3a14bfa27753befb3b62f5
Rbb666 <zhangbingru@rt-thread.com>
2025-02-05 16:38:15 +0800

    兼容5.1.0以下rtt版本

--- llm.c
+++ llm.c
@@ -339,7 +339,12 @@ static int llm2rtt(int argc, char **argv)
     handle.argc = argc;
     handle.get_answer = get_llm_answer;
 
+#if defined(RT_VERSION_CHECK) && (RTTHREAD_VERSION >= RT_VERSION_CHECK(5, 1, 0))
     rt_uint8_t prio = RT_SCHED_PRIV(rt_thread_self()).current_priority + 1;
+#else
+    rt_uint8_t prio = rt_thread_self()->current_priority + 1;
+#endif
+
     handle.thread = rt_thread_create("llm", llm_run, RT_NULL, LLM_THREAD_STACK_SIZE, prio, 10);
     if (handle.thread == RT_NULL)
     {
