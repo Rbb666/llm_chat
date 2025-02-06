bf70060b30d19a37e7c90efc9608b205f298b73c
Rbb666 <zhangbingru@rt-thread.com>
2025-02-06 20:05:50 +0800

    支持流式对话，响应更快

--- llm.h
+++ llm.h
@@ -12,24 +12,19 @@
 #define __LLM_H_
 
 #include <rtthread.h>
+#include <stdio.h>
+#include <string.h>
 
-#define LLM_USING_DEBUG
-#ifndef LLM_USING_DEBUG
+#ifndef PKG_LLMCHAT_DBG
 #define LLM_DBG(fmt, ...)
 #else
-#define LLM_DBG(fmt, ...)                \
-do{                                          \
-    rt_kprintf("[\033[32mLLM\033[0m] "); \
-    rt_kprintf(fmt, ##__VA_ARGS__);          \
+#define LLM_DBG(fmt, ...)                   \
+do{                                         \
+    rt_kprintf("[\033[32mLLM\033[0m] ");    \
+    rt_kprintf(fmt, ##__VA_ARGS__);         \
 }while(0)
 #endif
 
-#ifndef LLM_THREAD_STACK_SIZE
-    #define LLM_THREAD_STACK_SIZE 10240
-#endif
-#ifndef LLM_CMD_SIZE
-    #define LLM_CMD_SIZE 512
-#endif
 #ifndef LLM_HISTORY_LINES
     #define LLM_HISTORY_LINES 5
 #endif
@@ -43,18 +38,20 @@ enum llm_input_stat
 
 struct llm_obj
 {
-    rt_thread_t thread;
+    struct rt_thread thread;
+    rt_uint8_t thread_stack[PKG_LLM_THREAD_STACK_SIZE];
+
     struct rt_semaphore rx_sem;
 
     enum llm_input_stat stat;
 
-    int   argc;
+    int argc;
 
-    char llm_history[LLM_HISTORY_LINES][LLM_CMD_SIZE];
+    char llm_history[LLM_HISTORY_LINES][PKG_LLM_CMD_BUFFER_SIZE];
     rt_uint16_t history_count;
     rt_uint16_t history_current;
 
-    char line[LLM_CMD_SIZE];
+    char line[PKG_LLM_CMD_BUFFER_SIZE];
     rt_uint16_t line_position;
     rt_uint8_t line_curpos;
 
